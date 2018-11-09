c075240a08985e1606e20116777e43f0351ddbdf
kaka <murira.evans@gmail.com>
2018-11-09 20:42:05 +0300

    make the css and js file

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const text = 'A simple typing effect application'
+let i = 0
+
+const typing = ()=> {
+  if (i < text.length) {
+    document.getElementById('app').innerHTML += text.charAt(i)
+    i++;
+  } else if ( i = text.length) {
+    document.getElementById('app').innerHTML = ''
+    i = 0
+  }
+  setTimeout(typing,100)
+}
+
+typing();
